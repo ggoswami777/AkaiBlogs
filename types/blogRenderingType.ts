b7df0cce9392f3137b5957870674b7fd25efe142
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-06-15 21:48:31 +0530

    feat: define DocumentAST and block rendering types for blog content

---
+++ types/blogRenderingType.ts
@@ -0,0 +1,17 @@
+
+export type blockType= "paragraph" | "code" | "heading-1" | "heading-2";
+
+export type span={
+    text:string;
+    bold?:boolean;
+    italic?:boolean;
+    color?:string
+}
+
+export type block={
+    id:string;
+    type:blockType;
+    children:span[];
+}
+
+export type DocumentAST=block[];
