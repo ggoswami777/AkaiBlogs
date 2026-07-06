15f2d73e30bbdf5c03cb5502e4c11ac3d57f022c
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-07-06 21:28:47 +0530

    added chat validations

---
+++ lib/validations/chat.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+export const createConservationSchema=z.object({
+    receiverId:z.string().min(1,
+        "Reciever is required"
+    ),
+})
+
+export const sendMessageSchema=z.object({
+    conversationId:z.string().min(1,"Conversation is required"),
+    recieverId:z.string().min(1,"Reciever is required"),
+    content:z.string().trim().max(2000,"Message must be in 2000 characters or less").optional(),
+    sharedBlogId:z.string().optional(),
+}).refine(
+    (data)=>data.content || data.sharedBlogId,
+    {
+        message:"Message must contain text or a shared blog"
+    }
+)
+
+export const markReadSchema=z.object({
+    conversationId:z.string().min(1),
+})
